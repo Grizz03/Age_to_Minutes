9228e925b604b926c4d3a410cfd348d97b91cc8a
grizz03 <sawilson1110@yahoo.com>
2021-06-05 00:15:14 -0700

    added age to days

--- app/src/main/java/com/example/ageinminutes/MainActivity.kt
+++ app/src/main/java/com/example/ageinminutes/MainActivity.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -23,7 +22,6 @@ class MainActivity : AppCompatActivity() {
 
         myButton.setOnClickListener { view ->
             clickDatePicker(view)
-            Toast.makeText(this, "button works", Toast.LENGTH_SHORT).show()
         }
     }
 
@@ -34,12 +32,12 @@ class MainActivity : AppCompatActivity() {
         val month = myCalendar.get(Calendar.MONTH)
         val day = myCalendar.get(Calendar.DAY_OF_MONTH)
         DatePickerDialog(this,
-            { view,
+            { _,
               selectedYear,
               selectedMonth,
               selectedDayOfMonth ->
                 // Format for date
-                val userSelectedDate = "${selectedMonth+1}/$selectedDayOfMonth/$selectedYear"
+                val userSelectedDate = "${selectedMonth+1}/${selectedDayOfMonth}/$selectedYear"
                 // Create variable for text view
                 val mySelectedDate = findViewById<TextView>(R.id.selectedDate)
                 // Setting TextView to userSelectedDate string
@@ -51,11 +49,16 @@ class MainActivity : AppCompatActivity() {
 
                 // Converts date to milliseconds, divide by 60,000 to covert to minutes
                 val selectedDateInMinutes = theDate!!.time / 60000 // !! converts to non-null
-
-                // Show minutes
+                val currentDate = sdf.parse(sdf.format(System.currentTimeMillis()))
+                val currentDateToMinutes = currentDate!!.time / 60000
+                val differenceInMinutes = currentDateToMinutes - selectedDateInMinutes
+                // Convert to age in days
+                val days = differenceInMinutes / (60 * 24)
+                // alter textViews
                 val ageInMinutes = findViewById<TextView>(R.id.ageInMinutes)
-                ageInMinutes.text = selectedDateInMinutes.toString()
-            }, year, month, day
-        ).show()
+                val ageInDays = findViewById<TextView>(R.id.ageInDays)
+                ageInMinutes.text = differenceInMinutes.toString()
+                ageInDays.text = days.toString()
+            }, year, month, day).show()
     }
 }
